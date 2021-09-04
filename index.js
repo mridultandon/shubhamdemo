0a522122e45d0e14215c465e7ced29d30c1e2806
shuanand <shuanand@microsoft.com>
2021-09-04 12:04:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+var compression = require('compression')
+const app = express()
+const port = process.env.PORT||3000
+
+
+const shouldCompress = (req, res) => {
+    if (req.headers['x-no-compression']) {
+      // don't compress responses if this request header is present
+      return false;
+    }
+  
+    // fallback to standard compression
+    return compression.filter(req, res);
+  };
+
+app.use(compression({
+  // filter decides if the response should be compressed or not,
+  // based on the `shouldCompress` function above
+  filter: shouldCompress,
+  // threshold is the byte threshold for the response body size
+  // before compression is considered, the default is 1kb
+  threshold: 0
+}));
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
